02173350abab11b978ad50538eb0add2b1c476c4
DavidMirandaRosario <david.mirandapr@hotmail.com>
2014-10-20 17:29:15 -0400

    Push
    Push

---
+++ Testing.cpp
@@ -0,0 +1,21 @@
+//
+//  Testing.cpp
+//  Testing
+//
+//  Created by David Miranda Rosario on 9/10/14.
+//  Copyright (c) 2014 David Miranda Rosario. All rights reserved.
+//
+
+#include "Testing.h"
+#include <iostream>
+
+
+int main(){
+    
+    int ptr;
+    int *ptrptr;
+ 
+    std::cout << "Hello World";
+    
+    
+}
---
+++ Testing.h
@@ -0,0 +1,14 @@
+//
+//  Testing.h
+//  Testing
+//
+//  Created by David Miranda Rosario on 9/10/14.
+//  Copyright (c) 2014 David Miranda Rosario. All rights reserved.
+//
+
+#ifndef __Testing__Testing__
+#define __Testing__Testing__
+
+#include <iostream>
+
+#endif /* defined(__Testing__Testing__) */
